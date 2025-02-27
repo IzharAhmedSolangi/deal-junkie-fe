d7be81a21b8e3574311c8d24aca594eee93df65f
Umar Maqsood <128677464+UmarMaqsood123@users.noreply.github.com>
2025-02-27 12:20:51 +0500

    feat: base updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/deal-junkie-fe",
+  base: "/deal-junkie-fe/",
 });
