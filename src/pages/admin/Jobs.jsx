5fc788b02e118b14e18dc763149e6688fe1908b2
Umar Maqsood <128677464+UmarMaqsood123@users.noreply.github.com>
2025-03-11 15:42:52 +0500

    feat: jobs added in admin side

--- src/pages/admin/Jobs.jsx
+++ src/pages/admin/Jobs.jsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import useGetAllJobs from "../../services/admin/useGetAllJobs";
 import { ButtonLoader3 } from "../../components/shared/ButtonLoaders";
 import ShowMessage from "../../components/shared/ShowMessage";
+import { IoEyeOutline } from "react-icons/io5";
+import { CiCalendar, CiTimer } from "react-icons/ci";
 
 function Jobs() {
   const { GetAllJobs, jobs } = useGetAllJobs();
@@ -18,7 +20,37 @@ function Jobs() {
         </div>
         <div className="mt-3">
           {jobs.data && !jobs.loading && (
-            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4"></div>
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4">
+              {jobs.data?.map((item, index) => (
+                <div
+                  key={index}
+                  className="border border-[#15202712] rounded-[10px] shadow-md w-full h-[200px] p-3"
+                >
+                  <div>
+                    <div className="flex items-center gap-1">
+                      <div className="flex items-center gap-[2px] text-[#98A2B3] text-[14px] font-[500]">
+                        <CiCalendar />
+                        {item.expected_completion_date}
+                      </div>
+                      <div className="flex items-center gap-[2px] text-[#98A2B3] text-[14px] font-[500]">
+                        <CiTimer />
+                        11:59 PM
+                      </div>
+                    </div>
+                  </div>
+                  <h1 className="text-[#222222] text-[20px] font-[600] mt-3">
+                    {item.title}
+                  </h1>
+                  <p className="text-[#98A2B3] text-[16px] mt-1">
+                    {item.description}
+                  </p>
+                  <button className="bg-[#51B8EA0F] w-full h-[35px] border border-[#2D9ACF] rounded-sm text-[#2D9ACF] text-[13px] cursor-pointer flex justify-center items-center gap-1">
+                    <IoEyeOutline />
+                    View Details
+                  </button>
+                </div>
+              ))}
+            </div>
           )}
           {jobs.data && jobs.loading && (
             <div className="w-full flex justify-center mt-2">
