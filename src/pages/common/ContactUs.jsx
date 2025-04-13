e581d98e09bc45e31cc86ed3bb1a99b8e145668a
Umar Maqsood <128677464+UmarMaqsood123@users.noreply.github.com>
2025-04-14 01:10:26 +0500

    feat: animation added on contact us

--- src/pages/common/ContactUs.jsx
+++ src/pages/common/ContactUs.jsx
@@ -3,6 +3,8 @@ import * as Yup from "yup";
 import useContactUs from "../../services/common/useContactUs";
 import { ButtonLoader1 } from "../../components/shared/ButtonLoaders";
 import AppHead from "../../seo/AppHead";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const validationSchema = Yup.object({
   firstname: Yup.string()
@@ -53,6 +55,16 @@ function ContactUs() {
       });
     },
   });
+
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowForm(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
       <AppHead title="Contact Us - Deal Junkie" />
@@ -68,147 +80,187 @@ function ContactUs() {
           </h1>
         </div>
         <div className="w-full flex justify-center px-5 py-8">
-          <form onSubmit={handleSubmit} className="md:w-[80%] w-full">
-            <h1 className="font-[600] md:text-[32px] text-[26px] text-secondary">
-              Reach out to us
-            </h1>
-            <p className="font-[500] md:text-[16px] text-[12px] text-[#6F7487]">
-              Have a question or need assistance? Fill out the form below and
-              our dedicated support team will get back to you as soon as
-              possible. We&apos;re here to help!
-            </p>
-            <div className="w-full mt-4">
-              <div className="mt-2 grid grid-cols-2 gap-2">
-                <div>
-                  <input
-                    type="text"
-                    placeholder="First name"
-                    name="firstname"
-                    value={values.firstname}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.firstname && touched.firstname && (
-                    <p className="text-red-700 text-xs mt-1">
-                      {errors.firstname}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Last name"
-                    name="lastname"
-                    value={values.lastname}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.lastname && touched.lastname && (
-                    <p className="text-red-700 text-xs mt-1">
-                      {errors.lastname}
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div className="mt-3 grid grid-cols-2 gap-2">
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Email address"
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.email && touched.email && (
-                    <p className="text-red-700 text-xs mt-1">{errors.email}</p>
-                  )}
-                </div>
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Enter phone number"
-                    name="phone"
-                    value={values.phone}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.phone && touched.phone && (
-                    <p className="text-red-700 text-xs mt-1">{errors.phone}</p>
-                  )}
-                </div>
-              </div>
-              <div className="mt-3 grid grid-cols-2 gap-2">
-                <div>
-                  <input
-                    type="text"
-                    placeholder="State"
-                    name="state"
-                    value={values.state}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.state && touched.state && (
-                    <p className="text-red-700 text-xs mt-1">{errors.state}</p>
-                  )}
-                </div>
-                <div>
-                  <input
-                    type="text"
-                    placeholder="City"
-                    name="city"
-                    value={values.city}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.city && touched.city && (
-                    <p className="text-red-700 text-xs mt-1">{errors.city}</p>
-                  )}
-                </div>
-              </div>
-              <div className="mt-3 grid grid-cols-1">
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Address"
-                    name="address"
-                    value={values.address}
-                    onChange={handleChange}
-                    className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.address && touched.address && (
-                    <p className="text-red-700 text-xs mt-1">
-                      {errors.address}
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div className="mt-3 grid grid-cols-1">
-                <div>
-                  <textarea
-                    placeholder="Describe your query"
-                    type="text"
-                    name="query"
-                    value={values.query}
-                    onChange={handleChange}
-                    className="w-full min-h-[150px] max-h-[150px] px-3 py-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
-                  />
-                  {errors.query && touched.query && (
-                    <p className="text-red-700 text-xs mt-1">{errors.query}</p>
-                  )}
-                </div>
-              </div>
-              <div className="mt-3 w-full flex justify-end">
-                <button
-                  className="hover-slide-button bg-secondary cursor-pointer w-[150px] h-[40px] text-white rounded flex justify-center items-center"
-                  type="submit"
-                  disabled={loading}
-                >
-                  {loading ? <ButtonLoader1 /> : "Send Message"}
-                </button>
-              </div>
+          {!showForm ? (
+            <div className="w-full flex justify-center">
+              <svg
+                className="phoneSvg"
+                width="300px"
+                height="300px"
+                viewBox="0 0 32.666 32.666"
+              >
+                <motion.path
+                  strokeWidth={0.2}
+                  fill="none"
+                  initial={{ pathLength: 0 }}
+                  animate={{ pathLength: 1 }}
+                  transition={{ duration: 3 }}
+                  d="M28.189,16.504h-1.666c0-5.437-4.422-9.858-9.856-9.858l-0.001-1.664C23.021,4.979,28.189,10.149,28.189,16.504z
+                M16.666,7.856L16.665,9.52c3.853,0,6.983,3.133,6.981,6.983l1.666-0.001C25.312,11.735,21.436,7.856,16.666,7.856z M16.333,0
+                C7.326,0,0,7.326,0,16.334c0,9.006,7.326,16.332,16.333,16.332c0.557,0,1.007-0.45,1.007-1.006c0-0.559-0.45-1.01-1.007-1.01
+                c-7.896,0-14.318-6.424-14.318-14.316c0-7.896,6.422-14.319,14.318-14.319c7.896,0,14.317,6.424,14.317,14.319
+                c0,3.299-1.756,6.568-4.269,7.954c-0.913,0.502-1.903,0.751-2.959,0.761c0.634-0.377,1.183-0.887,1.591-1.529
+                c0.08-0.121,0.186-0.228,0.238-0.359c0.328-0.789,0.357-1.684,0.555-2.518c0.243-1.064-4.658-3.143-5.084-1.814
+                c-0.154,0.492-0.39,2.048-0.699,2.458c-0.275,0.366-0.953,0.192-1.377-0.168c-1.117-0.952-2.364-2.351-3.458-3.457l0.002-0.001
+                c-0.028-0.029-0.062-0.061-0.092-0.092c-0.031-0.029-0.062-0.062-0.093-0.092v0.002c-1.106-1.096-2.506-2.34-3.457-3.459
+                c-0.36-0.424-0.534-1.102-0.168-1.377c0.41-0.311,1.966-0.543,2.458-0.699c1.326-0.424-0.75-5.328-1.816-5.084
+                c-0.832,0.195-1.727,0.227-2.516,0.553c-0.134,0.057-0.238,0.16-0.359,0.24c-2.799,1.774-3.16,6.082-0.428,9.292
+                c1.041,1.228,2.127,2.416,3.245,3.576l-0.006,0.004c0.031,0.031,0.063,0.06,0.095,0.09c0.03,0.031,0.059,0.062,0.088,0.095
+                l0.006-0.006c1.16,1.118,2.535,2.765,4.769,4.255c4.703,3.141,8.312,2.264,10.438,1.098c3.67-2.021,5.312-6.338,5.312-9.719
+                C32.666,7.326,25.339,0,16.333,0z"
+                />
+              </svg>
             </div>
-          </form>
+          ) : (
+            <form onSubmit={handleSubmit} className="md:w-[80%] w-full">
+              <h1 className="font-[600] md:text-[32px] text-[26px] text-secondary">
+                Reach out to us
+              </h1>
+              <p className="font-[500] md:text-[16px] text-[12px] text-[#6F7487]">
+                Have a question or need assistance? Fill out the form below and
+                our dedicated support team will get back to you as soon as
+                possible. We&apos;re here to help!
+              </p>
+              <div className="w-full mt-4">
+                <div className="mt-2 grid grid-cols-2 gap-2">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="First name"
+                      name="firstname"
+                      value={values.firstname}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.firstname && touched.firstname && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.firstname}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Last name"
+                      name="lastname"
+                      value={values.lastname}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.lastname && touched.lastname && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.lastname}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-3 grid grid-cols-2 gap-2">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Email address"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.email && touched.email && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.email}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Enter phone number"
+                      name="phone"
+                      value={values.phone}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.phone && touched.phone && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.phone}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-3 grid grid-cols-2 gap-2">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="State"
+                      name="state"
+                      value={values.state}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.state && touched.state && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.state}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="City"
+                      name="city"
+                      value={values.city}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.city && touched.city && (
+                      <p className="text-red-700 text-xs mt-1">{errors.city}</p>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-3 grid grid-cols-1">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Address"
+                      name="address"
+                      value={values.address}
+                      onChange={handleChange}
+                      className="w-full h-[40px] px-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.address && touched.address && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.address}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-3 grid grid-cols-1">
+                  <div>
+                    <textarea
+                      placeholder="Describe your query"
+                      type="text"
+                      name="query"
+                      value={values.query}
+                      onChange={handleChange}
+                      className="w-full min-h-[150px] max-h-[150px] px-3 py-3 rounded-[4px] bg-transparent border border-[#02174C33] outline-none hover:border-secondary focus:border-secondary"
+                    />
+                    {errors.query && touched.query && (
+                      <p className="text-red-700 text-xs mt-1">
+                        {errors.query}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-3 w-full flex justify-end">
+                  <button
+                    className="hover-slide-button bg-secondary cursor-pointer w-[150px] h-[40px] text-white rounded flex justify-center items-center"
+                    type="submit"
+                    disabled={loading}
+                  >
+                    {loading ? <ButtonLoader1 /> : "Send Message"}
+                  </button>
+                </div>
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </>
