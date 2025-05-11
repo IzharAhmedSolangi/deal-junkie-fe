5cf252712c78f63d1c64b679798201321c2871d0
Umar Maqsood <128677464+UmarMaqsood123@users.noreply.github.com>
2025-05-11 17:42:49 +0500

    feat: limit exceed

--- src/pages/seller/dashboard/components/EditProfile.jsx
+++ src/pages/seller/dashboard/components/EditProfile.jsx
@@ -44,16 +44,16 @@ const validationSchema = Yup.object({
 
   describe_yourself: Yup.string()
     .required("Please describe yourself")
-    .max(300, "Limit exceeded"),
+    .max(1000, "Limit exceeded"),
   describe_skills_experience: Yup.string()
     .required("Please describe your skills and experience")
-    .max(300, "Limit exceeded"),
+    .max(1000, "Limit exceeded"),
   achievements: Yup.string()
     .required("Please describe your achievements")
-    .max(300, "Limit exceeded"),
+    .max(1000, "Limit exceeded"),
   work_preferences: Yup.string()
     .required("Please describe your work preferences")
-    .max(300, "Limit exceeded"),
+    .max(1000, "Limit exceeded"),
 });
 
 function EditProfile() {
@@ -139,110 +139,120 @@ function EditProfile() {
     <>
       {/* Personal Information */}
       <form onSubmit={handleSubmit} className="w-full">
-        <h1 className="font-semibold text-[30px] text-secondary">
-          Personal Information
-        </h1>
-        <div className="w-full mt-3">
-          <div className="mt-2 grid grid-cols-2 gap-2">
-            <div>
-              <Input
-                type="text"
-                placeholder="First name"
-                name="firstname"
-                value={values.firstname}
-                handleChange={handleChange}
-                icon={<FaRegUser />}
-              />
-              {errors.firstname && touched.firstname && (
-                <p className="text-red-700 text-xs mt-1">{errors.firstname}</p>
-              )}
+        {userInfo?.seller_profile !== null && (
+          <>
+            <h1 className="font-semibold text-[30px] text-secondary">
+              Personal Information
+            </h1>
+            <div className="w-full mt-3">
+              <div className="mt-2 grid grid-cols-2 gap-2">
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="First name"
+                    name="firstname"
+                    value={values.firstname}
+                    handleChange={handleChange}
+                    icon={<FaRegUser />}
+                  />
+                  {errors.firstname && touched.firstname && (
+                    <p className="text-red-700 text-xs mt-1">
+                      {errors.firstname}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="Last name"
+                    name="lastname"
+                    value={values.lastname}
+                    handleChange={handleChange}
+                    icon={<FaRegUser />}
+                  />
+                  {errors.lastname && touched.lastname && (
+                    <p className="text-red-700 text-xs mt-1">
+                      {errors.lastname}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="mt-3 grid grid-cols-2 gap-2">
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={values.email}
+                    handleChange={handleChange}
+                    icon={<MdOutlineEmail />}
+                  />
+                  {errors.email && touched.email && (
+                    <p className="text-red-700 text-xs mt-1">{errors.email}</p>
+                  )}
+                </div>
+                <div>
+                  <Input
+                    type="number"
+                    placeholder="Phone number"
+                    name="phone"
+                    value={values.phone}
+                    handleChange={handleChange}
+                    icon={<MdOutlinePhoneIphone />}
+                  />
+                  {errors.phone && touched.phone && (
+                    <p className="text-red-700 text-xs mt-1">{errors.phone}</p>
+                  )}
+                </div>
+              </div>
+              <div className="mt-3 grid grid-cols-2 gap-2">
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="City"
+                    name="city"
+                    value={values.city}
+                    handleChange={handleChange}
+                    icon={<FaCity />}
+                  />
+                  {errors.city && touched.city && (
+                    <p className="text-red-700 text-xs mt-1">{errors.city}</p>
+                  )}
+                </div>
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="State"
+                    name="state"
+                    value={values.state}
+                    handleChange={handleChange}
+                    icon={<FaCity />}
+                  />
+                  {errors.state && touched.state && (
+                    <p className="text-red-700 text-xs mt-1">{errors.state}</p>
+                  )}
+                </div>
+              </div>
+              <div className="mt-3 grid grid-cols-1">
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="Address"
+                    name="address"
+                    value={values.address}
+                    handleChange={handleChange}
+                    icon={<MdOutlineLocationOn />}
+                  />
+                  {errors.address && touched.address && (
+                    <p className="text-red-700 text-xs mt-1">
+                      {errors.address}
+                    </p>
+                  )}
+                </div>
+              </div>
             </div>
-            <div>
-              <Input
-                type="text"
-                placeholder="Last name"
-                name="lastname"
-                value={values.lastname}
-                handleChange={handleChange}
-                icon={<FaRegUser />}
-              />
-              {errors.lastname && touched.lastname && (
-                <p className="text-red-700 text-xs mt-1">{errors.lastname}</p>
-              )}
-            </div>
-          </div>
-          <div className="mt-3 grid grid-cols-2 gap-2">
-            <div>
-              <Input
-                type="text"
-                placeholder="Email"
-                name="email"
-                value={values.email}
-                handleChange={handleChange}
-                icon={<MdOutlineEmail />}
-              />
-              {errors.email && touched.email && (
-                <p className="text-red-700 text-xs mt-1">{errors.email}</p>
-              )}
-            </div>
-            <div>
-              <Input
-                type="number"
-                placeholder="Phone number"
-                name="phone"
-                value={values.phone}
-                handleChange={handleChange}
-                icon={<MdOutlinePhoneIphone />}
-              />
-              {errors.phone && touched.phone && (
-                <p className="text-red-700 text-xs mt-1">{errors.phone}</p>
-              )}
-            </div>
-          </div>
-          <div className="mt-3 grid grid-cols-2 gap-2">
-            <div>
-              <Input
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                handleChange={handleChange}
-                icon={<FaCity />}
-              />
-              {errors.city && touched.city && (
-                <p className="text-red-700 text-xs mt-1">{errors.city}</p>
-              )}
-            </div>
-            <div>
-              <Input
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
-                handleChange={handleChange}
-                icon={<FaCity />}
-              />
-              {errors.state && touched.state && (
-                <p className="text-red-700 text-xs mt-1">{errors.state}</p>
-              )}
-            </div>
-          </div>
-          <div className="mt-3 grid grid-cols-1">
-            <div>
-              <Input
-                type="text"
-                placeholder="Address"
-                name="address"
-                value={values.address}
-                handleChange={handleChange}
-                icon={<MdOutlineLocationOn />}
-              />
-              {errors.address && touched.address && (
-                <p className="text-red-700 text-xs mt-1">{errors.address}</p>
-              )}
-            </div>
-          </div>
-        </div>
+          </>
+        )}
         {/* Work Information */}
         {/* <div className="w-full mt-3">
           <h1 className="font-semibold text-[30px] text-secondary">
