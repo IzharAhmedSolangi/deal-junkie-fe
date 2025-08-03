4493e35db03c563db7775c621486089cdb3c4745
Umar Maqsood <128677464+UmarMaqsood123@users.noreply.github.com>
2025-08-04 00:12:56 +0500

    feat: null added

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -100,7 +100,7 @@ function Signup(props) {
               password: values.password,
               linkedin_link: values.linkedin_link,
               profile_picture: values.profile_picture,
-              referred_by: values.referred_by,
+              referred_by: values.referred_by ? values.referred_by : null,
               is_verified: true,
             },
             setAuthModalType
